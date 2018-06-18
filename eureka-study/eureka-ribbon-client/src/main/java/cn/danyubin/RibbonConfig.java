571bacf1aff34cb0b0b2c38905690e90b0881489
danyubin <danyubin1982@163.com>
2018-06-18 22:15:56 +0800

    eureka-client eureka-ribbon-client eureka-server
    Signed-off-by: danyubin <danyubin1982@163.com>

---
+++ eureka-study/eureka-ribbon-client/src/main/java/cn/danyubin/RibbonConfig.java
@@ -0,0 +1,17 @@
+package cn.danyubin;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RibbonConfig {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ eureka-study/eureka-ribbon-client/src/main/java/cn/danyubin/RibbonController.java
@@ -0,0 +1,29 @@
+package cn.danyubin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RibbonController {
+
+    @Autowired
+    RibbonService ribbonService;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam(required = false, defaultValue = "danyubin") String name) {
+        return ribbonService.hi(name);
+    }
+
+    @GetMapping("/testRibbon")
+    public String testRibbon() {
+        ServiceInstance instance = loadBalancerClient.choose("eureka-client");
+        return instance.getHost() + ":" + instance.getPort();
+    }
+}
